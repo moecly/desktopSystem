ff2ee20af8e401b77e54ba7fee5aac1f6da49929
moecly <1349269087ABC@gmail.com>
2023-05-18 09:49:01 +0800

    实现了videoPlayerPage的基本页面

--- videoPlayerPage/videoplayerpage.cpp
+++ videoPlayerPage/videoplayerpage.cpp
@@ -1,6 +1,122 @@
 #include "videoplayerpage.h"
+#include <QFileDialog>
 
 VideoPlayerPage::VideoPlayerPage(QWidget *parent) : QWidget(parent)
 {
+    layoutInit();
+    setVideoFileName(DEFAULT_VIDEO_FILE_NAME);
+    signalAndSlotInit();
+}
 
+int VideoPlayerPage::layoutInit() {
+    for (int i = 0; i < 2; i++) {
+        hBoxLayout[i] = new QHBoxLayout();
+        hBoxLayout[i]->setContentsMargins(0, 0, 0, 0);
+    }
+    hBoxLayout[0]->setAlignment(Qt::AlignCenter);
+    hBoxLayout[1]->setAlignment(Qt::AlignLeft);
+
+    for (int i = 0; i < 2; i++) {
+        hWidget[i] = new QWidget();
+        hWidget[i]->setLayout(hBoxLayout[i]);
+        hWidget[i]->setObjectName(QString("hWidget%1").arg(i));
+    }
+
+    videoWidget = new QVideoWidget();
+    listWidget = new QListWidget();
+    listWidget->setMinimumSize(100, 300);
+    listWidget->setMaximumWidth(200);
+    listWidget->setObjectName("listWidget");
+
+    vBoxLayout = new QVBoxLayout();
+    vBoxLayout->setContentsMargins(0, 0, 0, 0);
+    vBoxLayout->setAlignment(Qt::AlignCenter);
+    mainWidget = new QWidget(this);
+    mainWidget->setMinimumSize(MIN_WIDTH, MIN_HEIGHT);
+    mainWidget->setObjectName("mainWidget");
+    this->setMinimumSize(MIN_WIDTH, MIN_HEIGHT);
+
+    for (int i = 0; i < 2; i++) {
+        slider[i] = new QSlider(Qt::Horizontal);
+        slider[i]->setObjectName(QString("slider%1").arg(i));
+        slider[i]->setMaximumHeight(15);
+        slider[i]->setMinimumHeight(15);
+    }
+    slider[1]->setMaximumWidth(120);
+    slider[1]->setMinimumWidth(120);
+
+    for (int i = 0; i < 2; i++) {
+        label[i] = new QLabel();
+        label[i]->setObjectName(QString("label%1").arg(i));
+    }
+    label[0]->setText("00:00");
+    label[1]->setText("/00:00");
+
+    mainWidget->setLayout(vBoxLayout);
+    vBoxLayout->addWidget(hWidget[0]);
+    vBoxLayout->addWidget(slider[0]);
+    vBoxLayout->addWidget(hWidget[1]);
+
+    hBoxLayout[0]->addWidget(videoWidget);
+    hBoxLayout[0]->addWidget(listWidget);
+
+    for (int i = 0; i < 6; i++) {
+        pushButton[i] = new QPushButton();
+        pushButton[i]->setMinimumSize(60, 60);
+        pushButton[i]->setMaximumSize(60, 60);
+        pushButton[i]->setObjectName(QString("pushButton%1").arg(i));
+    }
+    pushButton[0]->setCheckable(true);
+    pushButton[5]->setCheckable(true);
+
+    QSpacerItem *hSpacer0 = new QSpacerItem(200, 60, QSizePolicy::Expanding, QSizePolicy::Maximum);
+    hBoxLayout[1]->addWidget(pushButton[0]);
+    hBoxLayout[1]->addWidget(pushButton[1]);
+    hBoxLayout[1]->addWidget(pushButton[2]);
+    hBoxLayout[1]->addWidget(slider[1]);
+    hBoxLayout[1]->addWidget(pushButton[3]);
+    hBoxLayout[1]->addWidget(pushButton[5]);
+    hBoxLayout[1]->addWidget(label[0]);
+    hBoxLayout[1]->addWidget(label[1]);
+    hBoxLayout[1]->addSpacerItem(hSpacer0);
+    hBoxLayout[1]->addWidget(pushButton[4]);
+
+    QFile file(":/videoPlayerPage/style/videoplayerpage.qss");
+    if (!file.exists() || !file.open(QIODevice::ReadOnly))
+        return -1;
+
+    QString styleSheet = file.readAll();
+    this->setStyleSheet(styleSheet);
+
+    file.close();
+
+    return 0;
+}
+
+void VideoPlayerPage::signalAndSlotInit() {
+    connect(pushButton[5], SIGNAL(clicked()), this, SLOT(selectVideoFileName()));
+}
+
+void VideoPlayerPage::selectVideoFileName() {
+    pushButton[5]->setChecked(true);
+    QString videoFileName = QFileDialog::getExistingDirectory();
+    pushButton[5]->setChecked(false);
+    if (videoFileName != "") {
+        setVideoFileName(videoFileName);
+        qDebug() << "videoFileName = " << videoFileName;
+    }
+}
+
+int VideoPlayerPage::setVideoFileName(QString videoFileName) {
+    QFile file(videoFileName);
+    if (!file.exists())
+        return -1;
+    videoFile.setFileName(videoFileName);
+    return 0;
+}
+
+void VideoPlayerPage::resizeEvent(QResizeEvent *event) {
+    Q_UNUSED(event)
+
+    mainWidget->resize(this->size());
 }
--- videoPlayerPage/videoplayerpage.h
+++ videoPlayerPage/videoplayerpage.h
@@ -2,16 +2,77 @@
 #define VIDEOPLAYERPAGE_H
 
 #include <QWidget>
+#include <utils/common.h>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QPushButton>
+#include <QMediaPlayer>
+#include <QMediaPlaylist>
+#include <QVideoWidget>
+#include <QSlider>
+#include <QListWidget>
+#include <QLabel>
+#include <QSpacerItem>
+#include <QFile>
+
+#define DEFAULT_VIDEO_FILE_NAME "~/"
 
 class VideoPlayerPage : public QWidget
 {
     Q_OBJECT
 public:
     explicit VideoPlayerPage(QWidget *parent = nullptr);
+    int setVideoFileName(QString videoFileName);
+
+private:
+    /*
+     * @params hBoxLayout[0]: 播放器和播放列表
+     * @params hBoxLayout[1]: 播放进度条下的按钮
+     */
+    QHBoxLayout *hBoxLayout[2];
+    QWidget *hWidget[2];
+
+    QVideoWidget *videoWidget;
+    QListWidget *listWidget;
+
+    /*
+     * @params slider[0]: 播放进度条
+     * @params slider[1]: 音量条
+     */
+    QSlider *slider[2];
+
+    /*
+     * @params label[0]: 当前播放时长
+     * @params label[1]: 视频总时长
+     */
+    QLabel *label[2];
+
+    /*
+     * @params pushButton[0]: 播放键
+     * @params pushButton[1]: 下一首
+     * @params pushButton[2]: 音量减
+     * @params pushButton[3]: 音量加
+     * @params pushButton[4]: 退出
+     * @params pushButton[5]: 选择文件路劲
+     */
+    QPushButton *pushButton[6];
+
+    /*
+     * 整体layout.
+     */
+    QVBoxLayout *vBoxLayout;
+    QWidget *mainWidget;
+
+    QFile videoFile;
+
+    int layoutInit();
+    void resizeEvent(QResizeEvent *event);
+    void signalAndSlotInit();
 
 signals:
 
 public slots:
+    void selectVideoFileName();
 };
 
 #endif // VIDEOPLAYERPAGE_H
